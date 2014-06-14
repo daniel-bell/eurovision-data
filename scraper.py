648492b398841dd87b047fab7b109afcffcf631b
Daniel Bell <me@daniel-bell.co.uk>
2014-06-14 09:58:30 +0100

    List of unique events now extracted from soup
    Extracts all table cells with class "event-entry" and then pulls
    the link from there into a set.

--- scraper.py
+++ scraper.py
@@ -1,12 +1,26 @@
 import urllib3
 from bs4 import BeautifulSoup
 
+# Download and parse main results history page
 eurovision_url = "http://www.eurovision.tv/page/history/year"
 http = urllib3.PoolManager()
-
 req = http.request("GET", eurovision_url)
-soup = BeautifulSoup(req.data)
-links = soup.findAll("a")
+soup = BeautifulSoup(req.data, "html.parser")
 
+# Extract links from only the history-by-year div
+cells = soup.findAll("td", {"class": "event-entry"})
+links = list()
+for cell in cells:
+    links.append(cell.find("a"))
+
+# Extract a links into a set to remove any duplicates
+events = set()
 for link in links:
-    print(link)
+    # Substring use to prevent extraction of unwanted links
+    if "/page/history/by-year/contest?event=" in link.get("href"):
+        events.add(link.get("href"))
+
+print(str(len(events)) + " events found")
+
+for event in events:
+    print(event)
